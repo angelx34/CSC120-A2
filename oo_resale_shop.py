19a335b15f86cbb2718d10c03faebf8e61596b9a
angelx34 <abritodiaz@smith.edu>
2023-02-07 14:38:08 -0500

    added a list to resale shop

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,29 @@
+from computer import *
+
 class ResaleShop:
 
-    # What attributes will it need?
+    # What attributes will it need? 
+    inventory: list
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+       
+    def __init__(self): 
+        self.inventory= []
+
+    def newComputer ( ) : 
+        
+
+    def printInventory(self):
+        #for c in self inventory
+            
+    
+        
+          
+            
+
+
+    pass # You'll remove this when you fill out your constructor
 
     # What methods will you need?
+   
