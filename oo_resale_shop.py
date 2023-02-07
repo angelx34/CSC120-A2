784a666efa3a711de864a77d9a030154ddc187f3
angelx34 <abritodiaz@smith.edu>
2023-02-07 16:38:36 -0500

    Added print and refurbished

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -14,10 +14,30 @@ class ResaleShop:
     def buy (self,description, processor_type,hard_drive_capacity,memory,operating_system, year_made, price) : 
         c= Computer(description, processor_type,hard_drive_capacity,memory,operating_system, year_made, price)
         self.inventory.append(c)
-    
 
-   # def printInventory(self):
-        #for c in self inventory
+    def sell (self,description, processor_type,hard_drive_capacity,memory,operating_system, year_made, price):
+        c= Computer(description, processor_type,hard_drive_capacity,memory,operating_system, year_made, price)
+        self.inventory.remove(c) 
+
+    def refurbish(self,item_id,new_os): 
+        for item_id in self.inventory : 
+            if item_id == self.inventory.index(c) : # locate the computer
+                if int(["year_made"]) < 2000:
+                    item_id["price"] = 0 # too old to sell, donation only
+                elif int(item_id["year_made"]) < 2012:
+                    item_id["price"] = 250 # heavily-discounted price on machines 10+ years old
+                elif int(item_id["year_made"]) < 2018:
+                    item_id["price"] = 550 # discounted price on machines 4-to-10 year old machines
+                else:
+                    item_id["price"] = 1000 # recent stuff
+                if new_os is not None:
+                    item_id["operating_system"] = new_os # update details after installing new OS
+            else:
+                print("Item", item_id, "not found. Please select another item to refurbish.")
+    
+    def printInventory(self,description, processor_type,hard_drive_capacity,memory,operating_system, year_made, price):
+        for c in self.inventory: 
+            c.print.Details(description, processor_type,hard_drive_capacity,memory,operating_system, year_made, price)
 
 def main():            
     myStore= ResaleShop()
