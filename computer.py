162355301b9344d40441a1fef6ae5180a19ae309
Your Name <you@example.com>
2023-02-08 17:28:18 -0500

    added comments to code

--- computer.py
+++ computer.py
@@ -1,6 +1,8 @@
 class Computer:
 
-    # What attributes will it need?
+    """
+    creates atribute variables
+    """
     description: str
     processor_type: str
     hard_drive_capacity: int
@@ -11,15 +13,21 @@ class Computer:
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
+    """
+    constructor containing the computers information
+    """
     def __init__(self, description, processor_type,hard_drive_capacity,memory,operating_system, year_made, price):
         self.description = description
         self.processor_type = processor_type
-        self.hard_drive_capacity: hard_drive_capacity
+        self.hard_drive_capacity = hard_drive_capacity
         self.memory = memory
         self.operating_system = operating_system
         self.year_made = year_made
         self.price = price
-
+    
+    """
+    prints the details of the computer
+    """
     def printDetails (self) :
         print (self.description)
         print (self.processor_type) 
@@ -29,6 +37,9 @@ class Computer:
         print (self.year_made)
         print (self.price)
 
+    """
+    updates price and operating system
+    """
     def update_price(self, new_price) : 
         self.price= new_price 
     def update_operating_system(self, New_OS) : 
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -7,18 +7,28 @@ class ResaleShop:
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-       
+    """
+    Creates a list called inventory to store the computers and the information abouth them.
+    """   
     def __init__(self): 
         self.inventory= []
-
+    """
+    Buy takes in a computer with all the information about it contained,
+    then appends it to the inventory 
+    """
     def buy (self,description, processor_type,hard_drive_capacity,memory,operating_system, year_made, price) : 
         c= Computer(description, processor_type,hard_drive_capacity,memory,operating_system, year_made, price)
         self.inventory.append(c)
-
+    """
+    Sell removes the called computer from the inventory
+    """
     def sell (self,description, processor_type,hard_drive_capacity,memory,operating_system, year_made, price):
         c= Computer(description, processor_type,hard_drive_capacity,memory,operating_system, year_made, price)
         self.inventory.remove(c) 
 
+    """
+    refurbish figures out the price of the computer based on year made. It also looks for a new os update and installs.
+    """
     def refurbish(self,c,new_os): 
         if c in self.inventory : 
             if c == self.inventory.index(c) : # locate the computer
@@ -35,17 +45,27 @@ class ResaleShop:
             else:   
                 print("Item", c, "not found. Please select another item to refurbish.")
     
+    """
+    calls the command in computer to print the details of the computer in inventory
+    """
     def printInventory(self):
         for c in self.inventory: 
-            c.print.Details() 
+            c.printDetails() 
 
+    """
+    calls command in computer to change the price to the value that refurbish set it at.
+    """
     def update_price(self,c, new_price) : 
         if c in self.inventory :
             self.computer.price= new_price
 
-    def update_price(self,c, new_OS) : 
+    """
+    updades the operating system  of computer
+    """
+    def update__operating_system(self,c, new_OS) : 
         if c in self.inventory :
             self.computer.operating_system= new_OS
+
 def main():            
     myStore =ResaleShop()
     myStore.printInventory()          
@@ -55,7 +75,3 @@ main()
             
 
 
- # You'll remove this when you fill out your constructor
-
-    # What methods will you need?
-   
